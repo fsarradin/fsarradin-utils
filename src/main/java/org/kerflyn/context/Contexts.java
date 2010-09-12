e5b16c1a55c4d058440b7cab722273f7c4c3428d
François Sarradin <fsarradin@gmail.com>
2010-09-13 01:46:40 +0200

    add null context

--- src/main/java/org/kerflyn/context/Contexts.java
+++ src/main/java/org/kerflyn/context/Contexts.java
@@ -2,7 +2,15 @@ package org.kerflyn.context;
 
 public class Contexts {
 
-    public static final Context NULL = null;
+    public static final Context NULL = new NullContextImpl();
+
+    private static class NullContextImpl implements Context {
+	public Context getParent() { return this; }
+	public void register(String name, Object o) {}
+	public Object get(String name) { return null; }
+	public <T> T get(String name, Class<T> contract) { return null; }
+	public <T> T get(Class<T> contract) { return null; }
+    }
 
 }
 
--- src/test/java/org/kerflyn/context/WhenNullContext.java
+++ src/test/java/org/kerflyn/context/WhenNullContext.java
@@ -1,5 +1,8 @@
 package org.kerflyn.context;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 import org.junit.Test;
 
 public class WhenNullContext {
@@ -7,6 +10,7 @@ public class WhenNullContext {
     @Test
 	public void thenTheParentIsNullAlso() {
 	Context context = Contexts.NULL;
+	assertThat(context.getParent(), is(Contexts.NULL));
     }
 
 }
