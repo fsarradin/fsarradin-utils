af1c1a0e14031038fbde05e9bcab473165a7861a
François Sarradin <fsarradin@gmail.com>
2010-09-13 01:22:20 +0200

    fill in the context interface

--- src/main/java/org/kerflyn/context/Context.java
+++ src/main/java/org/kerflyn/context/Context.java
@@ -2,6 +2,26 @@ package org.kerflyn.context;
 
 public interface Context {
 
+    Context getParent();
+
+    void register(String name, Object o);
+
+    /**
+     * Get an object from its name.
+     */
+    Object get(String name);
+
+    /**
+     * Get an object from its name and check if it is an instance of the
+     * given contract.
+     */
+    <T> T get(String name, Class<T> contract);
+
+    /**
+     * Get the first instance that fits the given contract.
+     */
+    <T> T get(Class<T> contract);
+
 }
 
 // End
