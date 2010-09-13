17f832c0c6893f4dfa6443013f82eec669e25d5a
François Sarradin <fsarradin@gmail.com>
2010-09-13 22:06:03 +0200

    - improve null context tests

--- src/test/java/org/kerflyn/context/WhenNullContext.java
+++ src/test/java/org/kerflyn/context/WhenNullContext.java
@@ -4,13 +4,35 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
+import org.junit.Rule;
+
+import org.junit.rules.ExpectedException;
 
 public class WhenNullContext {
+    
+    @Rule
+	public ExpectedException thrown = ExpectedException.none();
 
     @Test
 	public void thenTheParentIsNullAlso() {
 	Context context = Contexts.NULL;
 	assertThat(context.getParent(), is(Contexts.NULL));
+    }
+
+    @Test
+	public void thenComplainOnRegister() {
+	thrown.expect(UnsupportedOperationException.class);
+
+	Context context = Contexts.NULL;
+	context.register("object", "hello");
+    }
+
+    @Test
+	public void thenComplainOnGettingObject() {
+	thrown.expect(UnsupportedOperationException.class);
+
+	Context context = Contexts.NULL;
+	context.get(Object.class);
     }
 
 }
