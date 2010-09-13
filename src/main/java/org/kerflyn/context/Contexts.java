217d857ee108baef1044671611eb12e152699e2d
François Sarradin <fsarradin@gmail.com>
2010-09-13 22:50:15 +0200

    - add top-level context

--- src/main/java/org/kerflyn/context/Contexts.java
+++ src/main/java/org/kerflyn/context/Contexts.java
@@ -1,9 +1,53 @@
 package org.kerflyn.context;
 
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Map;
+
+
 public class Contexts {
 
     public static final Context NULL = new NullContextImpl();
 
+    public static Context create() {
+	return new ContextImpl(NULL);
+    }
+
+    private static class ContextImpl implements Context {
+	
+	private final Context parent;
+	private final Map<String, Object> objects;
+
+	public ContextImpl(Context parent) {
+	    this.parent = parent;
+	    objects = new HashMap<String, Object>();
+	}
+
+	public Context getParent() { return parent; }
+
+	public void register(String name, Object o) {
+	    objects.put(name, o);
+	}
+
+	public Object get(String name) {
+	    return objects.get(name);
+	}
+
+	public <T> T get(String name, Class<T> contract) {
+	    return contract.cast(objects.get(name));
+	}
+
+	public <T> T get(Class<T> contract) {
+	    for(Object o : objects.values()) {
+		if (contract.isInstance(o)) {
+		    return (T) o;
+		}
+	    }
+	    throw new NoSuchElementException();
+	}
+	
+    }
+    
     private static class NullContextImpl implements Context {
 	public Context getParent() { return this; }
 
--- src/test/java/org/kerflyn/context/WhenToplevelContext.java
+++ src/test/java/org/kerflyn/context/WhenToplevelContext.java
@@ -1,22 +1,70 @@
 package org.kerflyn.context;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 import org.junit.Rule;
+import org.junit.Before;
 
 import org.junit.rules.ExpectedException;
 
 public class WhenToplevelContext {
     
+    public Context context;
+    public Object object;
+
     @Rule
 	public ExpectedException thrown = ExpectedException.none();
 
+    @Before
+	public void setUp() {
+	context = Contexts.create();
+	object = java.util.Collections.emptyList();
+    }
+
     @Test
 	public void thenTheParentIsNull() {
-	Context context = Contexts.create();
 	assertThat(context.getParent(), is(Contexts.NULL));
+    }
+
+    @Test
+	public void thenGetObjectByName() {
+	context.register("object", object);
+	assertThat(context.get("object"), is(object));
+    }
+
+    @Test
+	public void thenGetObjectByNameAndByContract() {
+	context.register("object", object);
+	assertThat(context.get("object", List.class), is(object));
+	assertThat(context.get("object", Object.class), is(object));
+    }
+
+    @Test
+	public void thenComplainOnBadContract() {
+	thrown.expect(ClassCastException.class);
+
+	context.register("object", object);
+	context.get("object", String.class);
+    }
+
+    @Test
+	public void thenGetObjectByContract() {
+	context.register("object", object);
+	assertThat(context.get(List.class), is(object));
+	assertThat(context.get(Object.class), is(object));
+    }
+
+    @Test
+	public void thenComplainOnNoElementForTheContract() {
+	thrown.expect(NoSuchElementException.class);
+
+	context.register("object", object);
+	context.get(String.class);
     }
 
 }
